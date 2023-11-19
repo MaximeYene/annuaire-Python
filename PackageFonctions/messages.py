c673d933ea6bc39bb12e8d27ed64261074336935
Maxime Yene <maximeyene@gmail.com>
2023-11-19 02:01:23 +0100

    wip

--- PackageFonctions/messages.py
+++ PackageFonctions/messages.py
@@ -9,10 +9,19 @@ contacts=c.fetchall()
 
 
 def ajout_message():
-    nom_destinataire=input('A qui voulez vous envoyer un message? ')
-    for nom_destinataire in contacts:
-        message_envoyé=input('Entrez votre message : ')
-        c.execute("INSERT INTO messages(nom_destinataire, message_envoyé) VALUES (?, ?)",(nom_destinataire, message_envoyé))
+    nom_destinataire = input('A qui voulez-vous envoyer un message? ')
+    
+    c.execute("SELECT COUNT(*) FROM contacts WHERE nom=?", (nom_destinataire,))
+    existe = c.fetchone()[0]
+    
+    if existe:
+        message_envoyé = input('Entrez votre message : ')
+        c.execute("INSERT INTO messages(nom_destinataire, message_envoyé) VALUES (?, ?)", (nom_destinataire, message_envoyé))
         conn.commit()
+    else:
+        print("Il n'y a pas ce nom dans la liste.")
+
+
+
 
 
